6380d497c494aa5e1dd73f07325ffa4d1cca5ec0
envere <keithtxhh@gmail.com>
2019-05-24 23:05:07 +0800

    added a superficial navigation tab

--- FinTrack/App.js
+++ FinTrack/App.js
@@ -1,13 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import NavigationBar from './src/components/NavigationBar'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +15,14 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <NavigationBar />
       </View>
     );
   }
@@ -45,5 +44,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  navigations: {
+    alignItems: 'baseline',
   },
 });
---
+++ FinTrack/src/components/NavigationBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Text,
+} from 'native-base';
+
+export default class NavigationBar extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button vertical
+              active>
+              <Icon name="home" />
+              <Text>Home</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="stats" />
+              <Text>Portfolio</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="checkmark-circle-outline" />
+              <Text>Transactions</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
