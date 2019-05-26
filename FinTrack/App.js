7b898579e55397795f31f4cc1b502b98577ab33f
envere <keithtxhh@gmail.com>
2019-05-27 00:46:15 +0800

    implemented a splash screen

--- FinTrack/App.js
+++ FinTrack/App.js
@@ -3,6 +3,7 @@ import { Platform, StyleSheet, Text, View } from "react-native";
 import { Container, Header, Left, Body, Title } from "native-base";
 
 import NavigationBar from "./src/components/NavigationBar";
+import SplashScreen from "./src/pages/SplashScreen";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -12,7 +13,34 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isLoading: true };
+   }
+
+  performTimeConsumingTask = async () => {
+    return new Promise(resolve =>
+      setTimeout(() => {
+        resolve("result");
+      }, 2000)
+    );
+  };
+
+  async componentDidMount() {
+    // Preload data from an external API
+    // Preload data using AsyncStorage(?)
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
     return (
       <View style={styles.container}>
         <Container>
