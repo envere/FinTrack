408bfc6dde03874aab8747ab6bc91cb3ed72ec18
marcfyk <marcfong97@gmail.com>
2019-05-29 16:38:05 +0800

    added login and splash screen from master branch

---
+++ FinTrack/src/pages/LoginScreen.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+import LoginForm from "../components/LoginForm";
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <Text style={styles.mainText}>FinTrack Login</Text>
+        <LoginForm type="Login" />
+        <View style={styles.signupTextCont}>
+          <Text style={styles.signupText}>Don't have an account yet?</Text>
+          <TouchableOpacity>
+            <Text style={styles.signupButton}> Sign up!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#18325b",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  mainText: {
+      fontSize:40,
+      fontWeight:'bold',
+      color: 'white',
+      marginVertical: 80
+  },
+  signupTextCont: {
+    flexGrow: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingVertical: 16,
+    flexDirection: "row"
+  },
+  signupText: {
+    color: "rgba(255,255,255,0.6)",
+    fontSize: 16
+  },
+  signupButton: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "500"
+  }
+});
