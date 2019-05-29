7e523962db3fe2baa9126d1051ecda83db5bd7fe
envere <keithtxhh@gmail.com>
2019-05-30 00:26:21 +0800

    implemented app theme in NavBar

--- FinTrack/src/navigation/NavigationBar.js
+++ FinTrack/src/navigation/NavigationBar.js
@@ -7,31 +7,37 @@ import {
   FooterTab,
   Button,
   Icon,
-  Text
+  Text,
+  StyleProvider
 } from "native-base";
+
+import getTheme from "./../../native-base-theme/components";
+import FinTrackTheme from "./../../native-base-theme/variables/FinTrackTheme";
 
 export default class NavigationBar extends Component {
   render() {
     return (
-      <Container>
-        <Content />
-        <Footer>
-          <FooterTab>
-            <Button vertical active>
-              <Icon name="home" />
-              <Text>Home</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="stats" />
-              <Text>Portfolio</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="checkmark-circle-outline" />
-              <Text>Transactions</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <StyleProvider style={getTheme(FinTrackTheme)}>
+        <Container>
+          <Content />
+          <Footer>
+            <FooterTab>
+              <Button vertical active>
+                <Icon name="home" />
+                <Text>Home</Text>
+              </Button>
+              <Button vertical>
+                <Icon name="stats" />
+                <Text>Portfolio</Text>
+              </Button>
+              <Button vertical>
+                <Icon name="checkmark-circle-outline" />
+                <Text>Transactions</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      </StyleProvider>
     );
   }
 }
