1fe1ff14c43b825400284319dfe25f2f28e7b067
marcfyk <marcfong97@gmail.com>
2019-05-29 17:58:37 +0800

    added page header as abstraction class for all pages header

---
+++ FinTrack/src/components/PageHeader.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import { Header } from 'react-native-elements'
+
+import MenuButton from '../components/MenuButton'
+import SettingsButton from '../components/SettingsButton'
+
+export default class PageHeader extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Header 
+          leftComponent={<MenuButton navigation={this.props.navigation}/>}
+          centerComponent={{text: this.props.text, style: {color: 'white', fontSize: 28}}}
+          rightComponent={<SettingsButton navigation={this.props.navigation}/>}
+          backgroundColor='#18325b'
+          leftContainerStyle={{paddingLeft: 10, paddingBottom: 20}}
+          centerContainerStyle={{paddingBottom: 20}}
+          rightContainerStyle={{paddingRight: 10, paddingBottom: 20}}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
