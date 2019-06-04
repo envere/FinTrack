d198bd7d34ba17fcc6a2d08dfa67a0778fe3b4cf
Marc Fong <marcfong97@gmail.com>
2019-06-04 23:29:45 +0800

    edited App

--- backend/App.js
+++ backend/App.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const Express = require('express')
+const BodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
+const PORT = 3000
 const url = 'mongodb+srv://fintrack:nfactorialsorting@fintrack-snwv2.mongodb.net/test?retryWrites=true&w=majority'
 
-const app = express()
-const client = new MongoClient(url, { useNewUrlParser: true })
-client.connect(err => {
-  const collection = client.db('test').collection('devices')
-  console.log('connected')
-  client.close()
-  console.log('close')
+const app = Express()
+app.use(BodyParser.json())
+app.use(BodyParser.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+  console.log('listening')
+  const client = new MongoClient(url, { useNewUrlParser: true })
+  client.connect(err => {
+    console.log('mongoclient')
+    const collection = client.db('test').collection('devices')
+    client.close()
+  })
 })
