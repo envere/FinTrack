7c2663ca885fcd44a73b7fa2e6a34602a062ee2e
Marc Fong <marcfong97@gmail.com>
2019-06-16 21:23:26 +0800

    added local strategy to passport

--- backend/src/passport.js
+++ backend/src/passport.js
@@ -1,2 +1,24 @@
 const passport = require('passport')
-const localStrategy = require('passport-local')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/UserModel')
+
+const localStrategy = new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+  },
+  (username, password, callback) => {
+    return User
+      .findOne({username, password})
+      .then(user => {
+        return user
+          ? callback(null, user, { message: 'successful login' })
+          : callback(null, user, { message: 'incorrect username OR password' })
+      })
+      .catch(err => {
+        console.log(err)
+        callback(err)
+      })
+  }
+)
+
+passport.use(localStrategy)
