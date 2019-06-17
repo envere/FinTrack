f433094f5fe9c6934c690f75ff81577fc341fcc3
envere <keithtxhh@gmail.com>
2019-06-17 16:34:04 +0800

    fixed capitalisation

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,4 +1,4 @@
-const User = require('../models/userModel')
+const User = require('../models/UserModel')
 const express = require('express')
 const bcrypt = require('bcrypt')
 const router = express.Router()
