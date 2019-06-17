264fc5233e9108c9b0bddb25209feecb1efc1262
marcfyk <marcfong97@gmail.com>
2019-06-17 18:46:43 +0800

    added login page (w/o auth)

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -25,7 +25,7 @@ router.get('/', (req, res) => {
 
 router.get('/:username', (req, res) => {
   User
-    .findByUsername(req.params.username)
+    .findOne({username: req.params.username})
     .then(doc => {
       res.status(200).json({
         request: `${req.url}`,
@@ -46,32 +46,84 @@ router.post('/register', (req, res) => {
 
   const username = req.body.username
   const email = req.body.email
-  const password = req.body.password
+  const plaintext_password = req.body.password
 
-  const user = User.createUser(username, email, password)
+  const saltRounds = 10
 
-  user
-    .save()
-    .then(doc => {
-      res.status(200).json({
-        request: `${req.url}`,
-        message: 'added user',
-        user: doc,
-      })
-    })
-    .catch(err => {
-      res.status(500).json({
-        request: `${req.url}`,
+  bcrypt.hash(plaintext_password, saltRounds, (err, hash) => {
+    if (err) {
+      res.json({
+        request: req.url,
         message: 'error',
         error: err,
       })
-    })  
+    }
+    const user = new User({
+      username: username,
+      email: email,
+      password: hash,
+    })
+    user
+      .save()
+      .then(doc => {
+        res.status(200).json({
+          request: `${req.url}`,
+          message: 'added user',
+          user: doc,
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          request: `${req.url}`,
+          message: 'error',
+          error: err,
+        })
+      })
+  })
 })
 
 router.post('/login', (req, res) => {
-  const supplied_username = req.body.username
-  const supplied_password = req.body.password
+  const supplied_username = 'test_username'
+  const supplied_password = 'test_password'
 
+
+  User
+    .findOne({username: supplied_username})
+    .then(user => {
+      if (!user) {
+        res.status(404).json({
+          request: req.url,
+          message: 'error',
+          error: 'user account does not exist'
+        })
+      }
+      bcrypt.compare(supplied_password, user.password, (err, result) => {
+        if (!result) {
+          res.status(400).json({
+            request: req.url,
+            message: 'error',
+            error: 'invalid password',
+          })
+        }
+        
+      })
+    .catch(err => {
+      res.status(500).json({
+        request: req.url,
+        message: 'error',
+        error: err,
+      })
+    })
+
+  // User
+  //   .findByUsername('x')
+  //   .then(doc => {
+  //     if (!doc) {
+  //       throw new Error(`user with username: ${supplied_username} does not exist`)
+  //     }
+  //     return doc
+  //   })
+  //   .then(doc => doc.verifyPassword(supplied_password))
 })
 
 module.exports = router
