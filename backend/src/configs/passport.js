60b85ce96b57b211601e2bb8cdbff7c6a8e0b838
marcfyk <marcfong97@gmail.com>
2019-06-18 19:40:46 +0800

    updated passport file

--- backend/src/configs/passport.js
+++ backend/src/configs/passport.js
@@ -1,65 +1,26 @@
-import jwtSecret from './jwtConfig'
-import bcrypt from 'bcrypt'
-
-const saltRounds = 12
-
-const passport = require('passport')
-const passport_local = require('passport-local')
 const passport_jwt = require('passport-jwt')
-
-const localStrategy = passport_local.Strategy
 const jwtStrategy = passport_jwt.Strategy
-const ExtractJWT = passport_jwt.ExtractJWT
+const ExtractJwt = passport_jwt.ExtractJwt
 
+const mongoose = require('mongoose')
 const User = require('../models/UserModel')
 
-passport.use(
-  'login',
-  new localStrategy(
-    {
-      usernameField: 'username',
-      passwordField: 'password',
-      session: false,
-    },
-    (username, password, done) => {
-      User
-        .findOne({username: username})
-        .then(user => {
-          if (!user) {
-            return done(null, false, { message: 'username not found' })
-          } else {
-            bcrypt
-              .compare(password, user.password)
-              .then(isValid => {
-                return isValid
-                  ? done(null, user)
-                  : done(null, false, { message: 'invalid password' })
-              })
-          }
-        })
-        .catch(err => done(err))
-    }
-  )
-)
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'secret',
+}
 
-passport.use(
-  'jwt',
-  new jwtStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
-      secretOrKey: jwtSecret.secret,
-    },
-    (jwt_payload, done) => {
+module.exports = passport => {
+  passport.use(
+    new jwtStrategy(options, (payload, done) => {
       User
-        .findOne({username: jwt_payload.id})
+        .find({username: payload.id})
         .then(user => {
-          if (user) {
-            done(null, user)
-          } else {
-            done(null, false, { message: 'user not found' })
-          }
+          return user
+            ? done(null, user)
+            : done(null, false)
         })
-        .catch(err => done(err))
-    }
+        .catch(err => console.log(err))
+    })
   )
-)
+}
