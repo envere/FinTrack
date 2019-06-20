d78eac4f40838ca7454636fc23cb153da89685d9
marcfyk <marcfong97@gmail.com>
2019-06-21 01:00:04 +0800

    added adjusted time series

--- backend/src/alphavantage/adjusted_time_series_daily.js
+++ backend/src/alphavantage/adjusted_time_series_daily.js
@@ -10,3 +10,44 @@ function getEquity(symbol) {
       .catch(err => reject(err))
   })
 }
+
+function getTimeSeries(symbol) {
+  return new Promise((resolve, reject) => {
+    getEquity(symbol)
+      .then(data => resolve(data['Time Series (Daily)']))
+      .catch(err => reject(err))
+  })
+}
+
+function getDividend(symbol) {
+  return new Promise((resolve, reject) => {
+    getTimeSeries(symbol)
+      .then(timeSeries => {
+        const firstKey = Object.keys(timeSeries)[0]
+        const dividend = timeSeries[firstKey]['7. dividend amount']
+        resolve(dividend)
+      })
+      .catch(err => reject(err))
+  })
+}
+
+function initializeHistory(symbol) {
+  return new Promise((resolve, reject) => {
+    getTimeSeries(symbol)
+      .then(timeSeries => {
+        const deque = new Deque()
+        for (const key in timeSeries) {
+          deque.addLast(timeSeries[key]['7. dividend amount'])
+        }
+        resolve(deque)
+      })
+      .catch(err => reject(err))
+  })
+}
+
+module.exports = {
+  getEquity: getEquity,
+  getTimeSeries: getTimeSeries,
+  getDividend: getDividend,
+  initializeHistory: initializeHistory,
+}
