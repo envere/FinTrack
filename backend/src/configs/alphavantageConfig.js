992b9a2c5eefc821a775be0881e8928a14bb6554
marcfyk <marcfong97@gmail.com>
2019-06-21 00:48:16 +0800

    updated configs for alphavantage

--- backend/src/configs/alphavantageConfig.js
+++ backend/src/configs/alphavantageConfig.js
@@ -1,3 +1,9 @@
 module.exports = {
-  key: '2TU1SD6EZTVECMLR'
+  key: '2TU1SD6EZTVECMLR',
+  requestJSON: function(category, symbol) {
+    return `https://www.alphavantage.co/query?function=${category}&symbol=${symbol}&apikey=${this.key}`
+  },
+  requestCSV: function(category, symbol) {
+    return this.requestJSON(category, symbol) + '&datatype=csv'
+  },
 }
