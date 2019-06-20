b883e356f3fd50bec9555220c3f0b11ae9765e45
marcfyk <marcfong97@gmail.com>
2019-06-21 00:48:34 +0800

    added deque adt for history

--- backend/src/util/deque.js
+++ backend/src/util/deque.js
@@ -1,22 +1,37 @@
 module.exports = class {
   constructor() {
     this.arr = new Array()
+    this.limit = 10
   }
 
   addFirst(element) {
-    this.arr.unshift(element)
+    if (this.arr.length < this.limit) {
+      this.arr.unshift(element)
+    } else {
+      this.deleteLast()
+      this.arr.unshift(element)
+    }
+    return this
   }
 
   addLast(element) {
-    this.arr.push(element)
+    if (this.arr.length < this.limit) {
+      this.arr.push(element)
+    } else {
+      this.deleteLast()
+      this.arr.push(element)
+    }
+    return this
   }
 
   deleteFirst() {
     this.arr.shift()
+    return this
   }
 
   deleteLast() {
     this.arr.pop()
+    return this
   }
 
 }
