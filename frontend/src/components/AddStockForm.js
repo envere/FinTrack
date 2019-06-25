1b4df5d2d87faa51799d130605176c34dfc86bff
envere <keithtxhh@gmail.com>
2019-06-26 00:00:50 +0800

    implemented brokerage fees calculation

--- frontend/src/components/AddStockForm.js
+++ frontend/src/components/AddStockForm.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Modal, Dimensions, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Modal,
+  Dimensions,
+  Button
+} from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
+/**
+ * array indices:
+ * [0] for trade value of <=50k,
+ * [1] for 50k< value < 100k
+ * [2] for >= 100k
+ */
 const brokerageFeesList = {
   dbs: [0.0028, 0.0022, 0.0018],
   ocbc: [0.00275, 0.0022, 0.0018],
@@ -22,16 +35,21 @@ export default class AddStockForm extends Component {
   }
 
   updateFees() {
-    const brokerFees = brokerageFeesList[this.state.bank][0];
+    let brokerFees = brokerageFeesList[this.state.bank];
     const rawTotal = this.state.units * this.state.price;
-    /*if (rawTotal < 50000) {
+    if (rawTotal < 50000) {
       brokerFees = brokerFees[0];
     } else if (rawTotal < 100000) {
       brokerFees = brokerFees[1];
     } else {
       brokerFees = brokerFees[2];
-    }*/
-    this.setState({ fees: brokerFees * rawTotal });
+    }
+    let fees = brokerFees * rawTotal;
+    if (fees < 25) {
+      this.setState({ fees: 25 });
+    } else {
+      this.setState({ fees: fees});
+    }
     //return this.state.fees + rawTotal;
   }
   render() {
@@ -45,8 +63,7 @@ export default class AddStockForm extends Component {
         <TextInput
           placeholder="Units"
           onChangeText={text => {
-            this.setState({ units: parseFloat(text) });
-            this.updateFees();
+            this.setState({ units: parseFloat(text) }, () => this.updateFees());
           }}
           ref={input => (this.units = input)}
           onSubmitEditing={() => this.date.focus()}
@@ -60,24 +77,21 @@ export default class AddStockForm extends Component {
         <TextInput
           placeholder="Price"
           onChangeText={text => {
-            this.setState({ price: parseFloat(text) });
-            this.updateFees();
+            this.setState({ price: parseFloat(text) }, () => this.updateFees());
           }}
           ref={input => (this.price = input)}
           onSubmitEditing={() => this.fees.focus()}
         />
         <TextInput
           placeholder={
-            this.state.fees === 0 || this.state.fees === NaN
+            this.state.fees === 0 || isNaN(this.state.fees)
               ? "Trading fees"
               : this.state.fees.toString()
           }
           //onChangeText={text => this.setState({ fees: text })}
           ref={input => (this.fees = input)}
         />
-        <Button
-        title ="add"
-        onPress={()=>alert(this.state.fees)}/>
+        <Button title="add" onPress={() => alert(this.state.fees)} />
       </View>
     );
   }
