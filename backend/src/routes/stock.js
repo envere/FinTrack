b6ed17a921ac04e31843c5eac9d2b96c31672c0e
marcfyk <marcfong97@gmail.com>
2019-06-27 17:24:40 +0800

    added features to stock req

--- backend/src/routes/stock.js
+++ backend/src/routes/stock.js
@@ -28,6 +28,7 @@ function getStockName(symbol, res) {
               stock,
             })
             stock.save()
+            initPriceHistory(symbol)
           })
           .catch(err => res.sendStatus(404))
       }
@@ -35,50 +36,55 @@ function getStockName(symbol, res) {
     .catch(err => res.sendStatus(400))
 }
 
-function initHistory(symbol) {
-  alphavantage
-    .monthly
-    .prices(symbol)
-    .then(prices => {
-      const maxYear = prices[0].date.getFullYear()
-      const minYear = prices[prices.length - 1].date.getFullYear()
-
-      const years = []
-      for (let i = minYear; i <= maxYear; ++i) {
-        years.push(i)
-      }
-
-      const buckets = new Map()
-      years.forEach(year => buckets.set(year, new StockPrice({ symbol, year })))
-
-      prices.forEach(price => {
-        const year = price.date.getFullYear()
-        const month = price.date.getMonth()
-        const value = price.price
-        const bucket = buckets.get(year)
-        bucket.months.push({ date: month, price: value })
-      })
+function initPriceHistory(symbol) {
+  StockPrice
+    .deleteMany({ symbol })
+    .then(done => {
       alphavantage
-        .daily
+        .monthly
         .prices(symbol)
         .then(prices => {
+          const maxYear = prices[0].date.getFullYear()
+          const minYear = prices[prices.length - 1].date.getFullYear()
+
+          const years = []
+          for (let i = minYear; i <= maxYear; ++i) {
+            years.push(i)
+          }
+
+          const buckets = new Map()
+          StockPrice.deleteMany({ symbol })
+          years.forEach(year => buckets.set(year, new StockPrice({ symbol, year })))
+
           prices.forEach(price => {
             const year = price.date.getFullYear()
             const month = price.date.getMonth()
-            const date = price.date.getDate()
             const value = price.price
             const bucket = buckets.get(year)
-            bucket.days.push({ date: [month, date], price: value })
+            bucket.months.push({ date: month, price: value })
           })
-          return buckets
+          alphavantage
+            .daily
+            .prices(symbol)
+            .then(prices => {
+              prices.forEach(price => {
+                const year = price.date.getFullYear()
+                const month = price.date.getMonth()
+                const date = price.date.getDate()
+                const value = price.price
+                const bucket = buckets.get(year)
+                bucket.days.push({ date: [month, date], price: value })
+              })
+              return buckets
+            })
+            .then(buckets => buckets.forEach(stockprice => stockprice.save()))
+            .then(done => console.log(`initialized ${symbol} stock prices`))
+            .catch(err => console.log(err))
         })
-        .then(buckets => buckets.forEach(stockprice => stockprice.save()))
         .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 }
-
-initHistory('MSFT')
 
 router.post('/latestprice', (req, res) => {
   jwt
@@ -89,6 +95,24 @@ router.post('/latestprice', (req, res) => {
     })
 })
 
-router.post('/')
+router.post('/pricerange', (req, res) => {
+  jwt
+    .verifyJWT(req.token)
+    .then(auth => {
+      const startDate = req.body.start
+      const endDate = req.body.end
+      const years = [startDate.getFullYear()]
+      const months = [startDate.getMonth()]
+      const days = [startDate.getDate()]
+
+    })
+})
+
+StockPrice.findOne({ symbol: 'MSFT', year: 2019})
+  .then(stockprice => {
+    // console.log(stockprice)
+    console.log(stockprice.days)
+    // console.log(stockprice.months)
+  })
 
 module.exports = router
