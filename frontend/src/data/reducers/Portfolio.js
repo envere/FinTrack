96f47482b4003f8132eee1e5c286f8f7437b7f88
envere <keithtxhh@gmail.com>
2019-06-29 14:03:20 +0800

    added transactions schema, fixed date issues

--- frontend/src/data/reducers/Portfolio.js
+++ frontend/src/data/reducers/Portfolio.js
@@ -1,15 +1,30 @@
-const initialState = [];
+const initialState = [
+  [], //stocklist
+  [] //transaction history
+];
 const portfolio = (state = initialState, action) => {
   switch (action.type) {
     case "ADD":
       return [
-        ...state,
-        {
-          symbol: action.symbol,
-          name: action.name,
-          startPrice: action.startPrice,
-          currPrice: action.currPrice
-        }
+        [
+          //stocklist
+          ...state, // existing stocks
+          {
+            symbol: action.symbol,
+            name: action.name,
+            startPrice: action.startPrice,
+            currPrice: action.currPrice
+          }
+        ],
+        [
+          //transaction history
+          ...state, // existing transactions
+          {
+            symbol: action.symbol,
+            price: action.startPrice,
+            date: action.date
+          }
+        ]
       ];
     default:
       return state;
