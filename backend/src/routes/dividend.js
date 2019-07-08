c413829889eb8140e9d9b6a14aff57a907410a67
marcfyk <marcfong97@gmail.com>
2019-07-08 18:16:17 +0800

    dividend routes

--- backend/src/routes/dividend.js
+++ backend/src/routes/dividend.js
@@ -4,108 +4,65 @@ const DividendName = require('../models/DividendNameModel')
 const DividendPrice = require('../models/DividendPriceModel')
 const alphavantage = require('../util/alphavantage')
 const jwt = require('../util/jwt')
+const init = require('../util/initDB')
 const express = require('express')
 const router = express.Router()
 
-function initPriceHistory(symbol) {
-  StockPrice
-    .deleteMany({ symbol })
-    .then(done => {
-      alphavantage
-        .monthly
-        .prices(symbol)
-        .then(prices => {
-          const maxYear = prices[0].date.getFullYear()
-          const minYear = prices[prices.length - 1].date.getFullYear()
-
-          const years = []
-          for (let i = minYear; i <= maxYear; ++i) {
-            years.push(i)
-          }
-
-          const buckets = new Map()
-          years.forEach(year => buckets.set(year, new StockPrice({ symbol, year })))
-
-          prices.forEach(price => {
-            const year = price.date.getFullYear()
-            const month = price.date.getMonth()
-            const value = price.price
-            const bucket = buckets.get(year)
-            bucket.months.push({ date: price.date, price: value })
-          })
-          alphavantage
-            .daily
-            .prices(symbol)
-            .then(prices => {
-              prices.forEach(price => {
-                const year = price.date.getFullYear()
-                const month = price.date.getMonth()
-                const date = price.date.getDate()
-                const value = price.price
-                const bucket = buckets.get(year)
-                bucket.days.push({ date: price.date, price: value })
+router.post('/daily/dividend', (req, res) => {
+  jwt
+    .verifyJWT(req.token)
+    .then(auth => {
+      const symbol = req.body.symbol
+      const ISOdate = req.body.date
+      const year = ISOdate.split('-')[0]
+      DividendPrice
+        .findOne({ symbol, year })
+        .then(dividendprice => {
+          if (dividendprice) {
+            const filtered = dividendprice.days.filter(day => day.date.toISOString().split('T')[0] === ISOdate)
+            if (filtered.length === 0) {
+              res.sendStatus(404)
+            } else {
+              const dividend = filtered[0]
+              res.status(200).json({
+                message: `getting ${symbol} dividend on ${ISOdate}`,
+                dividend,
               })
-              return buckets
-            })
-            .then(buckets => buckets.forEach(stockprice => stockprice.save()))
-            .then(done => console.log(`initialized ${symbol} stock prices`))
-            .catch(err => console.log(err))
-        })
-        .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
-}
-
-function initDividendHistory(symbol) {
-  DividendPrice
-    .deleteMany({ symbol })
-    .then(done => {
-      alphavantage
-        .monthlyAdjusted
-        .dividends(symbol)
-        .then(dividends => {
-          const maxYear = dividends[0].date.getFullYear()
-          const minYear = dividends[dividends.length - 1].date.getFullYear()
-
-          const years = []
-          for (let i = minYear; i <= maxYear; ++i) {
-            years.push(i)
-          }
-
-          const buckets = new Map()
-          years.forEach(year => buckets.set(year, new DividendPrice({ symbol, year })))
-
-          dividends.forEach(dividend => {
-            const year = dividend.date.getFullYear()
-            const month = dividend.date.getMonth()
-            const value = dividend.price
-            const bucket = buckets.get(year)
-            bucket.months.push({ date: dividend.date, price: value })
-          })
-          alphavantage
-            .dailyAdjusted
-            .dividends(symbol)
-            .then(dividends => {
-              dividends.forEach(dividend => {
-                const year = dividend.date.getFullYear()
-                const month = dividend.date.getMonth()
-                const date = dividend.date.getDate()
-                const value = dividend.price
-                const bucket = buckets.get(year)
-                bucket.days.push({ date: dividend.date, price: value })
+            }
+          } else {
+            alphavantage
+              .dailyAdjusted
+              .dividends(symbol)
+              .then(dividends => {
+                const filtered = dividends.filter(dividend => dividend.date.toISOString().split('T')[0] === ISOdate)
+                if (filtered.length === 0) {
+                  res.sendStatus(404)
+                } else {
+                  const dividend = filtered[0]
+                  res.status(200).json({
+                    message: `getting ${symbol} dividend on ${ISOdate}`,
+                    dividend,
+                  })
+                  init(symbol)
+                }
               })
-              return buckets
-            })
-            .then(buckets => buckets.forEach(dividendprice => dividendprice.save()))
-            .then(done => console.log(`initialized ${symbol} dividends`))
-            .catch(err => console.log(err))
+              .catch(err => res.sendStatus(500))
+          }
         })
-        .catch(err => console.log(err))
+        .catch(err => res.sendStatus(500))
     })
-    .catch(err => console.log(err))
-}
+    .catch(err => res.sendStatus(403))
+})
 
-function init(symbol) {
-  initPriceHistory(symbol)
-  initDividendHistory(symbol)
-}
+router.post('/dividendrange', (req, res) => {
+  jwt
+    .verifyJWT(req.token)
+    .then(auth => {
+      const symbol = req.body.symbol
+      const start = new Date(req.body.start)
+      const end = new Date(req.body.end)
+      const startyear = start.getFullYear()
+      const endyear = end.getFullYear()
+    })
+    .catch(err => res.sendStatus(403))
+})
