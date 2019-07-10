b6b01717d2f4b71cb0d1c3fcc1d6b9637600cfb6
marcfyk <marcfong97@gmail.com>
2019-07-11 01:16:07 +0800

    reorganized jwt auth middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const jwt = require("./util/jwt");
+const express = require("express")
+const bodyParser = require("body-parser")
+const authenticate_access_JWT = require("./util/jwt").authenticate_access_JWT
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.originalUrl}`)
+  next()
+})
 
 app.use("/auth", require("./routes/auth"))
-app.use("/account", jwt.queryJWT, require("./routes/account"))
-app.use("/user", jwt.queryJWT, require("./routes/user"));
-app.use("/stock", jwt.queryJWT, require("./routes/stock"));
-app.use("/dividend", jwt.queryJWT, require("./routes/dividend"));
+app.use("/account", authenticate_access_JWT(),require("./routes/account"))
+app.use("/user", authenticate_access_JWT(), require("./routes/user"))
+app.use("/stock", authenticate_access_JWT(), require("./routes/stock"))
+app.use("/dividend", authenticate_access_JWT(), require("./routes/dividend"))
 
 app.use((req, res, next) => {
-  console.log(`${new Date().toString()} => ${req.originalUrl}`);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(404).send("error 404, Resource Not Found");
-});
+  res.status(404).send("error 404, Resource Not Found")
+})
 
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(500).send("error 500");
-});
+  res.status(500).send("error 500")
+})
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+  console.log(`listening on port ${PORT}`)
+})
