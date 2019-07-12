0b876fe83f2358fc2578210f1173cfb07ec9c69e
marcfyk <marcfong97@gmail.com>
2019-07-13 01:17:15 +0800

    added portfolio routes

--- backend/src/routes/portfolio.js
+++ backend/src/routes/portfolio.js
@@ -1,3 +1,9 @@
 const Portfolio = require('../models/portfolio-model')
 const express = require('express')
 const router = express.Router()
+
+router.get('/get', (req, res) => {
+  
+})
+
+module.exports = router
