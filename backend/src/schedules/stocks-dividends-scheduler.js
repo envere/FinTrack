3710f99b478f7a9aa2fc73a0ecff42c153937681
Marc Fong <marcfong97@gmail.com>
2019-07-16 15:10:11 +0800

    updated scheduler

--- backend/src/schedules/stocks-dividends-scheduler.js
+++ backend/src/schedules/stocks-dividends-scheduler.js
@@ -4,15 +4,8 @@ const SymbolName = require('../models/symbol-name-model')
 const StockPrice = require('../models/stock-price-model')
 const DividendPrice = require('../models/dividend-price-model')
 
-function getAllSymbols() {
-  return SymbolName
-    .find()
-    .then(symbolnames => symbolnames.map(symbolname => symbolname.symbol))
-    .catch(err => console.log(err))
-}
-
 function updateStock(symbol) {
-  StockPrice
+  return StockPrice
     .latest(symbol)
     .then(latest => {
       const days = latest.days
@@ -30,7 +23,7 @@ function updateStock(symbol) {
       }
       const newBuckets = []
       years.forEach(year => {
-        const stockprice = new StockPrice({symbol, year})
+        const stockprice = new StockPrice({ symbol, year })
         newBuckets.push(stockprice.save())
       })
       Promise
@@ -38,7 +31,7 @@ function updateStock(symbol) {
         .then(done => {
           const buckets = []
           buckets.push(StockPrice.latest(symbol))
-          years.forEach(year => buckets.push(StockPrice.findOne({symbol, year})))
+          years.forEach(year => buckets.push(StockPrice.findOne({ symbol, year })))
           return Promise.all(buckets)
         })
         .then(buckets => {
@@ -59,11 +52,11 @@ function updateStock(symbol) {
                 const year = bucket.year
                 const days = bucket.days
                 const months = bucket.months
-                return StockPrice.findOneAndUpdate({symbol, year}, {days, months})
+                return StockPrice.findOneAndUpdate({ symbol, year }, { days, months })
               })
               return Promise.all(updateBuckets)
             })
-            .then(done => console.log(`updated ${symbol}'s stock\n${done}`))
+            .then(done => console.log(`updated ${symbol}'s prices`))
             .catch(err => console.log(err))
         })
         .catch(err => console.log(err))
@@ -72,22 +65,70 @@ function updateStock(symbol) {
 }
 
 function updateDividend(symbol) {
-
+  return DividendPrice
+    .latest(symbol)
+    .then(latest => {
+      const days = latest.days
+      let latestDate = days[0].date
+      days.forEach(day => {
+        if (day.date >= latestDate) {
+          latestDate = day.date
+        }
+      })
+      const latestYear = latestDate.getFullYear()
+      const nowYear = (new Date()).getFullYear()
+      const years = []
+      for (let i = latestYear + 1; i <= nowYear; ++i) {
+        years.push(i)
+      }
+      const newBuckets = []
+      years.forEach(year => {
+        const dividendprice = new DividendPrice({ symbol, year })
+        newBuckets.push(dividendprice.save())
+      })
+      Promise
+        .all(newBuckets)
+        .then(done => {
+          const buckets = []
+          buckets.push(DividendPrice.latest(symbol))
+          years.forEach(year => buckets.push(DividendPrice.findOne({ symbol, year })))
+          return Promise.all(buckets)
+        })
+        .then(buckets => {
+          const ISOdate = latestDate.toISOString().split('T')[0]
+          const ISOnow = (new Date()).toISOString().split('T')[0]
+          const days = alphavantage.dailyAdjusted.dividends_range(symbol, ISOdate, ISOnow)
+          const months = alphavantage.monthlyAdjusted.dividends_range(symbol, ISOdate, ISOnow)
+          Promise
+            .all([days, months])
+            .then(dividends => {
+              const days = dividends[0]
+              const months = dividends[1]
+              const map = new Map()
+              buckets.forEach(bucket => map.set(bucket.year, bucket))
+              days.forEach(dividend => map.get(dividend.date.getFullYear()).days.push(dividend))
+              months.forEach(dividend => map.get(dividend.date.getFullYear()).months.push(dividend))
+              const updateBuckets = buckets.map(bucket => {
+                const year = bucket.year
+                const days = bucket.days
+                const months = bucket.months
+                return DividendPrice.findOneAndUpdate({ symbol, year }, { days, months })
+              })
+              return Promise.all(updateBuckets)
+            })
+            .then(done => console.log(`updated ${symbol}'s dividends`))
+            .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 }
+
 
 function update(symbol) {
-  const stock = () => updateStock(stock)
-  const dividend = () => updateDividend(stock)
   Promise
-    .all([stock(), dividend()])
-    .then(`${symbol}'s update completed`)
-    .catch(err => console.log(err))
+    .all([updateStock(symbol), updateDividend(symbol)])
+    .catch(err => console.log(`update error ${symbol}`))
 }
 
-function updateAll() {
-  getAllSymbols()
-    .then(symbols => symbols.forEach(symbol => update(symbol)))
-    .catch(err => console.log(err))
-}
-
-updateStock('MSFT')
+update('D05.SI')
