3a7499f939cf4d23c06c82ce6a67bae9886c90e3
envere <keithtxhh@gmail.com>
2019-07-22 19:12:15 +0900

    removed flatlist in favor of accordion style for clarity

--- frontend/src/pages/PortfolioScreen.js
+++ frontend/src/pages/PortfolioScreen.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import {
+  ListItem,
+  Left,
+  Right,
+  renderLeftHiddenRow,
+  renderRightHiddenRow,
+  Accordion
+} from "native-base";
 
 import PageHeader from "../components/PageHeader";
 import BottomTab from "../navigation/BottomTab";
@@ -21,7 +29,27 @@ export default class PortfolioScreen extends Component {
     );
   }
 
+  calculatePercentage(stock) {
+    const startPrice = stock.investedCapital;
+    const endPrice = stock.currentValue;
+    const percentage = ((endPrice / startPrice - 1) * 100).toFixed(2);
+    return percentage >= 0 ? `+${percentage}%` : `-${percentage * -1}%`;
+  }
+
   render() {
+    const accordionData = this.state.stockData.map(stock => {
+      const data = {
+        title: `${stock.symbol} (${this.calculatePercentage(stock)})\n${
+          stock.name
+        }`,
+        content: `Units: ${stock.units}\nCapital Invested: $${
+          stock.investedCapital.toFixed(2)
+        }\nCurrent Value: $${stock.currentValue.toFixed(2)}\nAverage Buying Price: $${(
+          stock.investedCapital / stock.units
+        ).toFixed(4)}`
+      };
+      return data;
+    });
     return (
       <View style={styles.container}>
         <PageHeader text="Portfolio" navigation={this.props.navigation} />
@@ -36,26 +64,26 @@ export default class PortfolioScreen extends Component {
           <Text style={styles.header}>Stock</Text>
           <Text style={styles.header}>Profit/Loss</Text>
         </View>
-        <FlatList
-          data={this.state.stockData}
-          renderItem={({ item }) => (
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                width: "80%",
-                marginHorizontal: "10%"
-              }}
-            >
-              <View>
-                <Text>{JSON.stringify(item)}</Text>
-              </View>
-              <View />
-            </View>
-          )}
-          keyExtractor={(item, index) => index.toString()} // pasted from SO to fix bugs
-        />
+        <View style={styles.list}>
+        {/*}  <FlatList
+            data={this.state.stockData}
+            renderItem={({ item }) => (
+              <ListItem>
+                <Left>
+                  <View>
+                    <Text>{item.symbol}</Text>
+                    <Text>{item.name}</Text>
+                  </View>
+                </Left>
+                <Right>
+                  <Text>{this.calculatePercentage(item)}</Text>
+                </Right>
+              </ListItem>
+            )}
+            keyExtractor={(item, index) => index.toString()} // pasted from SO to fix bugs
+            />*/}
+          <Accordion dataArray={accordionData} expanded={0} />
+        </View>
         <Button
           title="Refresh"
           onPress={
@@ -76,6 +104,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F5FCFF"
+  },
+  list: {
+    height: "73.5%",
+    width: "90%",
+    marginHorizontal: "5%"
   },
   header: {
     fontWeight: "bold",
