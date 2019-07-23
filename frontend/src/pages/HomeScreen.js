9e800289735122085837705f8c3208c9a4dd079a
envere <keithtxhh@gmail.com>
2019-07-23 17:07:30 +0900

    added visualisation for asset allocation

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -5,20 +5,41 @@ import {
   Button,
   Text,
   Modal,
-  Dimensions,
+  Dimensions
 } from "react-native";
+import { VictoryPie, VictoryChart, VictoryTheme } from "victory-native";
+import Svg from "react-native-svg";
 
 import PageHeader from "../components/PageHeader";
 import BottomTab from "../navigation/BottomTab";
 import AddStockForm from "../components/AddStockForm";
+import store from "../data/PortfolioStore";
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalVisible: false,
-      searchSymbol: ""
+      pieData: null
     };
+  }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      const data = store.getState().stockList;
+      const totalCapital = data
+        .map(stock => stock.investedCapital)
+        .reduce((acc, val) => acc + val);
+      const makePercent = num => ((num / totalCapital) * 100).toFixed(1) + "%";
+      this.setState({
+        pieData: data.map(stock => {
+          return {
+            x: `${stock.symbol}\n(${makePercent(stock.investedCapital)})`,
+            y: stock.investedCapital
+          };
+        })
+      });
+    });
   }
 
   setModalVisible(visible) {
@@ -35,8 +56,17 @@ export default class HomeScreen extends Component {
           visible={this.state.modalVisible}
           onRequestClose={() => this.setModalVisible(!this.state.modalVisible)}
         >
-          <AddStockForm setModalVisible={this.setModalVisible.bind(this)}/>
+          <AddStockForm setModalVisible={this.setModalVisible.bind(this)} />
         </Modal>
+        <VictoryPie
+          data={this.state.pieData}
+          innerRadius={80}
+          padAngle={2}
+          labelRadius={93}
+          style={{
+            labels: { fill: "white", fontSize: 15, fontWeight: "bold" }
+          }}
+        />
         <Button
           title="Add stock"
           onPress={() => {
