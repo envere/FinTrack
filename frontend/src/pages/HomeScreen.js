f9859b0415b81cab503e6545f222d653b3db4739
envere <keithtxhh@gmail.com>
2019-07-29 13:24:36 +0800

    abstracted visualisation calculations

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -30,6 +30,142 @@ export default class HomeScreen extends Component {
     };
   }
 
+  calculatePieData(data) {
+    const totalCapital = data.stockList.reduce(
+      (acc, val) => acc + val.investedCapital,
+      0
+    );
+    const makePercent = num => ((num / totalCapital) * 100).toFixed(1) + "%";
+    this.setState({
+      pieData: data.stockList.map(stock => {
+        return {
+          x: `${stock.symbol}\n(${makePercent(stock.investedCapital)})`,
+          y: stock.investedCapital
+        };
+      })
+    });
+  }
+
+  calculateTransactionData(data) {
+    const transactionsArr = data.transactions
+      .slice()
+      .reverse()
+      .map(transaction => [transaction.tradeValue])
+      .reduce((acc, curr) => acc.concat(acc[acc.length - 1] + curr[0]));
+    const dates = data.transactions
+      .slice()
+      .reverse()
+      .map(transaction => new Date(transaction.date.substring(0, 10)));
+    let transactionsData = [];
+    for (i = 0; i < dates.length; i++) {
+      transactionsData[i] = {
+        y: transactionsArr[i],
+        x: dates[i]
+      };
+    }
+    this.setState({
+      transactionsData: transactionsData
+    });
+  }
+
+  calculatePortfolioData(data) {
+    this.setState({
+      priceHistory: [],
+      priceHistoryLoaded: false
+    });
+    const unitsPerDay = data.transactions
+      .slice()
+      .reverse()
+      .map(transactions => {
+        return {
+          symbol: transactions.symbol,
+          units: transactions.units,
+          date: transactions.date
+        };
+      })
+      .reduce((acc, curr) => {
+        if (!acc.map(stock => stock.symbol).includes(curr.symbol)) {
+          return acc.concat(curr);
+        }
+        const units = acc
+          .filter(stock => stock.symbol === curr.symbol)
+          .reduce((acc, curr) => acc + curr.units, 0);
+        const updatedTransaction = {
+          symbol: curr.symbol,
+          units: curr.units + units,
+          date: curr.date
+        };
+        return acc.concat(updatedTransaction);
+      }, []);
+
+    const uniqueList = unitsPerDay.reduce(
+      (acc, curr) =>
+        acc.map(stock => stock.symbol).includes(curr.symbol)
+          ? acc
+          : [...acc, curr],
+      []
+    );
+
+    uniqueList.forEach(stock => {
+      const bodyReq = {
+        start: stock.date.substring(0, 10),
+        end: new Date().toISOString().substring(0, 10),
+        symbol: stock.symbol
+      };
+      const priceRangeApi =
+        "https://orbital-fintrack.herokuapp.com/stock/pricerange";
+      fetch(priceRangeApi, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + this.state.token
+        },
+        body: JSON.stringify(bodyReq)
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (
+            !this.state.priceHistory
+              .map(stock => stock.message)
+              .includes(res.message)
+          ) {
+            const history = res.prices.days.map(priceDay => {
+              const units = unitsPerDay.find(
+                transaction =>
+                  transaction.date <= priceDay.date &&
+                  transaction.symbol === stock.symbol
+              ).units;
+              return {
+                price: priceDay.price * units,
+                date: priceDay.date
+              };
+            });
+
+            const newArr = this.state.priceHistory
+              .concat(history)
+              .reduce((acc, curr) => {
+                if (acc.map(stock => stock.date).includes(curr.date)) {
+                  return acc.map(stock => {
+                    if (stock.date === curr.date) {
+                      return {
+                        price: stock.price + curr.price,
+                        date: stock.date
+                      };
+                    }
+                    return stock;
+                  });
+                }
+                return acc.concat(curr);
+              }, []);
+            this.setState({
+              priceHistory: newArr,
+              priceHistoryLoaded: true
+            });
+          }
+        });
+    });
+  }
+
   componentDidMount() {
     RNSecureStorage.get("accessToken").then(val =>
       this.setState({
@@ -39,142 +175,9 @@ export default class HomeScreen extends Component {
 
     this.unsubscribe = store.subscribe(() => {
       const data = store.getState();
-
-      // pieData calculations
-      const totalCapital = data.stockList
-        .map(stock => stock.investedCapital)
-        .reduce((acc, val) => acc + val, 0);
-      const makePercent = num => ((num / totalCapital) * 100).toFixed(1) + "%";
-
-      // transactionsData calculations
-      const transactionsArr = data.transactions
-        .slice()
-        .reverse()
-        .map(transaction => [transaction.tradeValue])
-        .reduce((acc, curr) => acc.concat(acc[acc.length - 1] + curr[0]));
-      const dates = data.transactions
-        .slice()
-        .reverse()
-        .map(transaction => new Date(transaction.date.substring(0, 10)));
-      let transactionsData = [];
-      for (i = 0; i < dates.length; i++) {
-        transactionsData[i] = {
-          y: transactionsArr[i],
-          x: dates[i]
-        };
-      }
-      if (transactionsData.length === 1) {
-        const newData = transactionsData.slice();
-        transactionsData = transactionsData.concat(newData)
-      }
-
-      // portfolioData calculations
-      const unitsPerDay = data.transactions
-        .slice()
-        .reverse()
-        .map(transactions => {
-          return {
-            symbol: transactions.symbol,
-            units: transactions.units,
-            date: transactions.date
-          };
-        })
-        .reduce((acc, curr) => {
-          if (!acc.map(stock => stock.symbol).includes(curr.symbol)) {
-            return acc.concat(curr);
-          }
-          const units = acc
-            .filter(stock => stock.symbol === curr.symbol)
-            .reduce((acc, curr) => acc + curr.units, 0);
-          const updatedTransaction = {
-            symbol: curr.symbol,
-            units: curr.units + units,
-            date: curr.date
-          };
-          return acc.concat(updatedTransaction);
-        }, []);
-
-      const uniqueList = unitsPerDay.reduce(
-        (acc, curr) =>
-          acc.map(stock => stock.symbol).includes(curr.symbol)
-            ? acc
-            : [...acc, curr],
-        []
-      );
-
-      uniqueList.forEach(stock => {
-        const bodyReq = {
-          start: stock.date.substring(0, 10),
-          end: new Date().toISOString().substring(0, 10),
-          symbol: stock.symbol
-        };
-        const priceRangeApi =
-          "https://orbital-fintrack.herokuapp.com/stock/pricerange";
-        fetch(priceRangeApi, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + this.state.token
-          },
-          body: JSON.stringify(bodyReq)
-        })
-          .then(res => res.json())
-          .then(res => {
-            if (
-              !this.state.priceHistory
-                .map(stock => stock.message)
-                .includes(res.message)
-            ) {
-              const history = res.prices.days
-                .sort((a, b) =>
-                  a.date > b.date ? 1 : b.date > a.date ? -1 : 0
-                )
-                .map(priceDay => {
-                  const units = unitsPerDay.find(
-                    transaction =>
-                      transaction.date <= priceDay.date &&
-                      transaction.symbol === stock.symbol
-                  ).units;
-                  return {
-                    price: priceDay.price * units,
-                    date: priceDay.date
-                  };
-                });
-
-              const newArr = this.state.priceHistory
-                .concat(history)
-                .reduce((acc, curr) => {
-                  if (acc.map(stock => stock.date).includes(curr.date)) {
-                    return acc.map(stock => {
-                      if (stock.date === curr.date) {
-                        return {
-                          price: stock.price + curr.price,
-                          date: stock.date
-                        };
-                      }
-                      return stock;
-                    });
-                  }
-                  return acc.concat(curr);
-                }, []);
-              console.log(newArr);
-              this.setState({
-                priceHistory: newArr,
-                priceHistoryLoaded: true
-              });
-            }
-          });
-      });
-
-      this.setState({
-        pieData: data.stockList.map(stock => {
-          return {
-            x: `${stock.symbol}\n(${makePercent(stock.investedCapital)})`,
-            y: stock.investedCapital
-          };
-        }),
-        transactionsData: transactionsData,
-      });
+      this.calculatePieData(data);
+      this.calculateTransactionData(data);
+      this.calculatePortfolioData(data);
     });
   }
 
@@ -208,15 +211,22 @@ export default class HomeScreen extends Component {
       return (
         <View>
           <VictoryChart scale={{ x: "time" }}>
-            {/*<VictoryLine
-              animate={{ duration: 500 }}
-              interpolation="stepAfter"
-              style={{
-                data: { stroke: "navy" },
-                parent: { border: "2px solid #ccc" }
-              }}
-              data={this.state.priceHistory}
-            />*/}
+            {
+              <VictoryLine
+                animate={{ duration: 500 }}
+                interpolation="stepAfter"
+                style={{
+                  data: { stroke: "navy" },
+                  parent: { border: "2px solid #ccc" }
+                }}
+                data={this.state.priceHistory.map(point => {
+                  return {
+                    x: new Date(point.date.substring(0, 10)),
+                    y: point.price
+                  };
+                })}
+              />
+            }
             <VictoryLine
               animate={{ duration: 500 }}
               interpolation="stepAfter"
@@ -224,13 +234,28 @@ export default class HomeScreen extends Component {
                 data: { stroke: "orange" },
                 parent: { border: "2px solid #ccc" }
               }}
-              data={this.state.transactionsData}
+              data={
+                /*this.state.transactionsData.length === 1
+                  ? this.state.transactionsData.concat([
+                      {
+                        x: new Date(),
+                        y: this.state.transactionsData[0].y
+                      }
+                    ])
+                  :*/ this.state.transactionsData.concat(
+                  [{ x: new Date(), y: 10000 }]
+                )
+              }
             />
           </VictoryChart>
         </View>
       );
     }
-    return <View><Text>Please wait while the graph is being generated...</Text></View>
+    return (
+      <View>
+        <Text>Please wait while the graph is being generated...</Text>
+      </View>
+    );
   }
 
   toggleGraph(boolean) {
@@ -266,9 +291,12 @@ export default class HomeScreen extends Component {
         <Button
           title="Add stock"
           onPress={() => {
-            this.setModalVisible(true);
+            //this.setModalVisible(true);
             //alert(JSON.stringify(this.state.portfolioData));
             //alert(JSON.stringify(this.state.priceHistory));
+            alert(JSON.stringify(this.state.transactionsData.concat(
+              [{ x: new Date(), y: 10000 }]
+            )));
           }}
         />
         <BottomTab />
